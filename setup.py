b53b428fd99aaa416a4bfb8e12ef04649499918f
mskomek <mskomek@gmail.com>
2025-01-23 17:25:53 +0300

    Added differences.patch and README

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
-import os
+from distutils.core import setup
 
-from setuptools import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(name="pysmithplot",
-      version="0.2.0",
-      packages=["smithplot"],
-      description="An extension for Matplotlib providing a projection class to generate high quality Smith Chart plots.",
-      long_description=read('README.md'),
-      author="Paul Staerke",
-      author_email="paul.staerke@gmail.com",
-      license="BSD",
-      url="https://github.com/vMeijin/pySmithPlot",
-      install_requires=["matplotlib >= 1.2.0", "numpy", "scipy"])
+setup(
+    name='pySmithPlot-Updated',
+    version='0.2.1',
+    packages=['smithplot'],
+    url='https://github.com/mskomek/pySmithPlot-Updated',
+    license='BSD',
+    author='mskomek',
+    author_email='',
+    description='An extension for Matplotlib providing a projection class to generate high quality Smith Chart plots.'
+)
